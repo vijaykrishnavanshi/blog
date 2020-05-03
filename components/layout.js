4a3535a02b9ad2915fc9e78055e1ad3a9cadf43e
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2020-05-03 14:53:34 +0530

    Fixed meta information

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Personal blog"
         />
         <meta
           property="og:image"
